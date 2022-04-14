5ff0765eefe3153f3d4cf56b8c96edf7c65c02da
mathilde_h <mathilde.humeau@yahoo.com>
2022-04-14 18:18:11 +0100

    Fetched list of GitHub repositories

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // select the space where profile information will be displayed
 const overview = document.querySelector(".overview");
 
+// select the unordered list to display the repos list
+const repoDisplay = document.querySelector(".repo-list")
+
 const userName = "mathum49";
 const getGitHubProfile = async function(){
     const getData = await fetch(`https://api.github.com/users/${userName}`);
@@ -26,4 +29,23 @@ const displayGitHubData = function (data){
           <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
         </div>`;
     overview.append(userInfo);
+    getRepo();
+}
+
+const getRepo = async function (){
+    const getRepoData = await fetch(`https://api.github.com/users/${userName}/repos?per_page=100&sort=updated`);
+    const repoData = await getRepoData.json();
+    displayRepos(repoData);
+    // console.log(repoData);
+};
+
+// getRepo();
+
+const displayRepos = function(repos){
+    for(const repo of repos){
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`;
+        repoDisplay.append(li);
+    }
 }
